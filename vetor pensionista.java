b15059e22e3e4b057ddda563169ad2dcfaf70a6b
Diogo Pincetta <diogohe08@gmail.com>
2024-02-15 22:27:57 -0300

    Exercicio Pensionista

---
+++ vetor pensionista.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+	  Scanner sc = new Scanner(System.in);
+	  
+	 Product[] vect = new Product[10];
+	 
+	System.out.println("How many rooms will be rend");
+	int n = sc.nextInt();
+	
+	for(int i = 1; i<=n; i++) {
+		System.out.println("Rent #" + i + ":");
+		System.out.println("Name");
+		sc.nextLine();
+		String name = sc.nextLine();
+		System.out.println("Email:");
+		String email = sc.next();
+		System.out.print("Room");
+		int roomnumber = sc.nextInt();
+		
+		vect[roomnumber] = new Product (name, email);
+		
+		}
+	System.out.println();
+	System.out.println("Busy rooms:");
+	for(int i=0; i<10;i++) {
+		if(vect[i] != null) {
+			System.out.println(i + ":" + vect[i]);
+		}
+	}
+	
+	sc.close(); 
+	}
+	  
+	
+}
+	
+
